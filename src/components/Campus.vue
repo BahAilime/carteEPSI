a3417f85c46b086fb6d64294358009549576dd7c
rmerceron <rmerceron@gmail.com>
2024-09-24 15:09:57 +0200

    Add View components

---
+++ src/components/Campus.vue
@@ -0,0 +1,223 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="levels">
+    <div class="level level--1" aria-label="Level 1">
+      <img src="../../img/rdc.png" alt="">
+      <div class="level__pins">
+        <a class="pin pin--1-1" data-category="1" data-space="1.01" href="#" aria-label="Pin for Apple Heart">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--appleheart"><use xlink:href="#icon-appleheart"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--1-2" data-category="1" data-space="1.02" href="#" aria-label="Pin for Crazy Banana">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--bananas"><use xlink:href="#icon-bananas"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--1-3" data-category="2" data-space="1.03" href="#" aria-label="Pin for Folding Life">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--origami"><use xlink:href="#icon-origami"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--1-4" data-category="3" data-space="1.04" href="#" aria-label="Pin for Dress me not">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--dress"><use xlink:href="#icon-dress"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--1-5" data-category="4" data-space="1.05" href="#" aria-label="Pin for Meditation Garden">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--tree2"><use xlink:href="#icon-tree2"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--1-6" data-category="1" data-space="1.06" href="#" aria-label="Pin for Seed of Life">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--avocado"><use xlink:href="#icon-avocado"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--1-7" data-category="1" data-space="1.07" href="#" aria-label="Pin for Raw Delight">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--cake"><use xlink:href="#icon-cake"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--1-8" data-category="1" data-space="1.08" href="#" aria-label="Pin for The Wizard">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--mushroom"><use xlink:href="#icon-mushroom"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--1-9" data-category="1" data-space="1.09" href="#" aria-label="Pin for Smoothies &amp; Soul">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--pear"><use xlink:href="#icon-pear"></use></svg>
+									</span>
+        </a>
+      </div>
+      <!-- /level__pins -->
+    </div>
+    <div class="level level--2" aria-label="Level 2">
+      <img src="../../img/E1.png" alt="">
+      <div class="level__pins">
+        <a class="pin pin--2-1" data-category="1" data-space="2.01" href="#" aria-label="Pin for Grilled Chipotle">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--pepper"><use xlink:href="#icon-pepper"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--2-2" data-category="2" data-space="2.02" href="#" aria-label="Pin for Rocketship Tech">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--rocket"><use xlink:href="#icon-rocket"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--2-3" data-category="2" data-space="2.03" href="#" aria-label="Pin for Which Bug?">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--bug"><use xlink:href="#icon-bug"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--2-4" data-category="3" data-space="2.04" href="#" aria-label="Pin for Cognitio">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--books"><use xlink:href="#icon-books"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--2-5" data-category="1" data-space="2.05" href="#" aria-label="Pin for The Eggplanthead">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--eggplant"><use xlink:href="#icon-eggplant"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--2-6" data-category="1" data-space="2.06" href="#" aria-label="Pin for Superfood">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--strawberry"><use xlink:href="#icon-strawberry"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--2-7" data-category="3" data-space="2.07" href="#" aria-label="Pin for No Princess">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--heart"><use xlink:href="#icon-heart"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--2-8" data-category="3" data-space="2.08" href="#" aria-label="Pin for Tool Exchange">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--wrench"><use xlink:href="#icon-wrench"></use></svg>
+									</span>
+        </a>
+      </div>
+      <!-- /level__pins -->
+    </div>
+    <div class="level level--3" aria-label="Level 3">
+      <img src="../../img/E2.png" alt="">
+      <div class="level__pins">
+        <a class="pin pin--3-1" data-category="1" data-space="3.01" href="#" aria-label="Pin for Oolong House">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--tea"><use xlink:href="#icon-tea"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--3-2" data-category="2" data-space="3.02" href="#" aria-label="Pin for Enlightend Path">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--pyramid"><use xlink:href="#icon-pyramid"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--3-3" data-category="3" data-space="3.03" href="#" aria-label="Pin for What makes us walk">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--shoe"><use xlink:href="#icon-shoe"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--3-4" data-category="3" data-space="3.04" href="#" aria-label="Pin for Your Last Shirt">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--shirt"><use xlink:href="#icon-shirt"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--3-5" data-category="4" data-space="3.05" href="#" aria-label="Pin for Breathe">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--tree"><use xlink:href="#icon-tree"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--3-6" data-category="4" data-space="3.06" href="#" aria-label="Pin for Feel the Grass">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--grass"><use xlink:href="#icon-grass"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--3-7" data-category="4" data-space="3.07" href="#" aria-label="Pin for Star Gazer">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--star"><use xlink:href="#icon-star"></use></svg>
+									</span>
+        </a>
+      </div>
+      <!-- /level__pins -->
+    </div>
+    <div class="level level--4" aria-label="Level 4">
+      <img src="../../img/E3.png" alt="">
+      <div class="level__pins">
+        <a class="pin pin--4-1" data-category="1" data-space="4.01" href="#" aria-label="Pin for Cold Soup Kitchen">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--tomato"><use xlink:href="#icon-tomato"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--4-2" data-category="2" data-space="4.02" href="#" aria-label="Pin for Docu Dome">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--cinema"><use xlink:href="#icon-cinema"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--4-3" data-category="3" data-space="4.03" href="#" aria-label="Pin for Little Artist">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--paint-brush"><use xlink:href="#icon-paint-brush"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--4-4" data-category="4" data-space="4.04" href="#" aria-label="Pin for Sacred Symmetry">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--modx"><use xlink:href="#icon-modx"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--4-5" data-category="4" data-space="4.05" href="#" aria-label="Pin for Hot Tub Festival">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--droplet"><use xlink:href="#icon-droplet"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--4-6" data-category="2" data-space="4.06" href="#" aria-label="Pin for Love 2 Cook">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--cupcake"><use xlink:href="#icon-cupcake"></use></svg>
+									</span>
+        </a>
+        <a class="pin pin--4-7" data-category="2" data-space="4.07" href="#" aria-label="Pin for Permaculture Hub">
+									<span class="pin__icon">
+										<svg class="icon icon--pin"><use xlink:href="#icon-pin"></use></svg>
+										<svg class="icon icon--logo icon--leafcircle"><use xlink:href="#icon-leafcircle"></use></svg>
+									</span>
+        </a>
+      </div>
+      <!-- /level__pins -->
+    </div>
+  </div>
+  <!-- /levels -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SearchList.vue
@@ -0,0 +1,74 @@
+<template>
+  <aside class="spaces-list" id="spaces-list">
+    <div class="search">
+      <input class="search__input" placeholder="Search..." />
+      <button class="boxbutton boxbutton--darker close-search" aria-label="Close details">
+        <svg class="icon icon--cross">
+          <use xlink:href="#icon-cross"></use>
+        </svg>
+      </button>
+    </div>
+    <span class="label">
+      <input id="sort-by-name" class="label__checkbox" type="checkbox" aria-label="Show alphabetically"/>
+      <label class="label__text">A - Z</label>
+    </span>
+    <ul class="list grouped-by-category">
+      <li
+          v-for="(item, index) in spacesList"
+          :key="index"
+          class="list__item"
+          :data-level="item.level"
+          :data-category="item.category"
+          :data-space="item.space"
+      >
+        <a href="#" class="list__link">{{ item.name }}</a>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      spacesList: [
+        { name: 'The Wizard', level: 1, category: 1, space: 1.08 },
+        { name: 'Apple Heart', level: 1, category: 1, space: 1.01 },
+        { name: 'Crazy Banana', level: 1, category: 1, space: 1.02 },
+        { name: 'The Eggplanthead', level: 2, category: 1, space: 2.05 },
+        { name: 'Smoothies & Soul', level: 1, category: 1, space: 1.09 },
+        { name: 'Seed of Life', level: 1, category: 1, space: 1.06 },
+        { name: 'Raw Delight', level: 1, category: 1, space: 1.07 },
+        { name: 'Grilled Chipotle', level: 2, category: 1, space: 2.01 },
+        { name: 'Oolong House', level: 3, category: 1, space: 3.01 },
+        { name: 'Cold Soup Kitchen', level: 4, category: 1, space: 4.01 },
+        { name: 'Superfood', level: 2, category: 1, space: 2.06 },
+        { name: 'Love 2 Cook', level: 4, category: 2, space: 4.06 },
+        { name: 'Permaculture Hub', level: 4, category: 2, space: 4.07 },
+        { name: 'Folding Life', level: 1, category: 2, space: 1.03 },
+        { name: 'Rocketship Tech', level: 2, category: 2, space: 2.02 },
+        { name: 'Which Bug?', level: 2, category: 2, space: 2.03 },
+        { name: 'Enlightened Path', level: 3, category: 2, space: 3.02 },
+        { name: 'Docu Dome', level: 4, category: 2, space: 4.02 },
+        { name: 'Little Artist', level: 4, category: 3, space: 4.03 },
+        { name: 'Your Last Shirt', level: 3, category: 3, space: 3.04 },
+        { name: 'Tool Exchange', level: 2, category: 3, space: 2.08 },
+        { name: 'Dress me not', level: 1, category: 3, space: 1.04 },
+        { name: 'Cognitio', level: 2, category: 3, space: 2.04 },
+        { name: 'What makes us walk', level: 3, category: 3, space: 3.03 },
+        { name: 'No Princess', level: 2, category: 3, space: 2.07 },
+        { name: 'Star Gazer', level: 3, category: 4, space: 3.07 },
+        { name: 'Space 16', level: 4, category: 4, space: 4.04 },
+        { name: 'Breathe', level: 3, category: 4, space: 3.05 },
+        { name: 'Meditation Garden', level: 1, category: 4, space: 1.05 },
+        { name: 'Hot Tub Festival', level: 4, category: 4, space: 4.05 },
+        { name: 'Feel the Grass', level: 3, category: 4, space: 3.06 }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Add your CSS here */
+</style>
